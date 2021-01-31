6cec35c8819a4119566205f7a4e077ca19765bb6
Samuel Carriere <carriere911@gmail.com>
2021-01-30 21:47:04 -0800

    initial project code

---
+++ Source/PlanetShooter/PlanetShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlanetShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLANETSHOOTER_API APlanetShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
